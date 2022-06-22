80556636b33a726dc18e41243b026aa45df01c06
Wrood Alrashidi <wrood4747@gmail.com>
2022-06-22 19:55:58 +0300

    code done

---
+++ Day1-gameDev/Day1-gameDev/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+// Hero
+int heroHeight = 100;
+int heroAge = 99;
+string heroSuperPower = "Genius coder";
+
+Console.WriteLine("I am " + heroHeight + " meters tall ,  I am " + heroAge + " years old. And my super power is a " + heroSuperPower);
+
+//Villian
+int vililanHeight = 200;
+int villainAge = 6;
+string villainSuperPower = "Bad coder";
+
+Console.WriteLine("I am " + vililanHeight + " meters tall ,  I am " + villainAge + " years old. And my super power is a " + villainSuperPower);
+
+// age difference
+int ageDifference = heroAge - villainAge;
+Console.WriteLine(ageDifference);
+
+// bonus
+heroHeight = heroHeight + 5;
+Console.WriteLine(heroHeight);
+
+string villainSuperPower = "none";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
